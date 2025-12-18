90f48165a5176049b4d075a96b890172908c80f5
Lucy Lee <Lucelle21@hotmail.com>
2025-12-18 16:32:56 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace StockPriceRecord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.cmbRange = new System.Windows.Forms.ComboBox();
+            this.cmbSymbol = new System.Windows.Forms.ComboBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.nudPrice = new System.Windows.Forms.NumericUpDown();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.txtSymbol = new System.Windows.Forms.TextBox();
+            this.btnPlot = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.grid);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1078, 644);
+            this.splitContainer1.SplitterDistance = 300;
+            this.splitContainer1.TabIndex = 12;
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.RowHeadersVisible = false;
+            this.grid.RowHeadersWidth = 62;
+            this.grid.RowTemplate.Height = 31;
+            this.grid.Size = new System.Drawing.Size(300, 644);
+            this.grid.TabIndex = 11;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.chart1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 220F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(774, 644);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(3, 223);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(768, 418);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.btnPlot);
+            this.panel1.Controls.Add(this.dtpTo);
+            this.panel1.Controls.Add(this.dtpFrom);
+            this.panel1.Controls.Add(this.cmbRange);
+            this.panel1.Controls.Add(this.cmbSymbol);
+            this.panel1.Controls.Add(this.btnImport);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.nudPrice);
+            this.panel1.Controls.Add(this.dtpDate);
+            this.panel1.Controls.Add(this.txtSymbol);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(768, 214);
+            this.panel1.TabIndex = 13;
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Location = new System.Drawing.Point(206, 92);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(200, 29);
+            this.dtpTo.TabIndex = 17;
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Location = new System.Drawing.Point(206, 59);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(200, 29);
+            this.dtpFrom.TabIndex = 16;
+            // 
+            // cmbRange
+            // 
+            this.cmbRange.FormattingEnabled = true;
+            this.cmbRange.Location = new System.Drawing.Point(206, 33);
+            this.cmbRange.Name = "cmbRange";
+            this.cmbRange.Size = new System.Drawing.Size(121, 26);
+            this.cmbRange.TabIndex = 15;
+            // 
+            // cmbSymbol
+            // 
+            this.cmbSymbol.FormattingEnabled = true;
+            this.cmbSymbol.Location = new System.Drawing.Point(206, 3);
+            this.cmbSymbol.Name = "cmbSymbol";
+            this.cmbSymbol.Size = new System.Drawing.Size(121, 26);
+            this.cmbSymbol.TabIndex = 14;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(83, 97);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 29);
+            this.btnImport.TabIndex = 13;
+            this.btnImport.Text = "匯入CSV";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(0, 94);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(77, 35);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "新增";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // nudPrice
+            // 
+            this.nudPrice.DecimalPlaces = 2;
+            this.nudPrice.Location = new System.Drawing.Point(0, 64);
+            this.nudPrice.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.nudPrice.Name = "nudPrice";
+            this.nudPrice.Size = new System.Drawing.Size(120, 29);
+            this.nudPrice.TabIndex = 11;
+            this.nudPrice.Value = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(0, 33);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 29);
+            this.dtpDate.TabIndex = 10;
+            // 
+            // txtSymbol
+            // 
+            this.txtSymbol.Location = new System.Drawing.Point(3, 4);
+            this.txtSymbol.Name = "txtSymbol";
+            this.txtSymbol.Size = new System.Drawing.Size(100, 29);
+            this.txtSymbol.TabIndex = 9;
+            // 
+            // btnPlot
+            // 
+            this.btnPlot.Location = new System.Drawing.Point(206, 127);
+            this.btnPlot.Name = "btnPlot";
+            this.btnPlot.Size = new System.Drawing.Size(75, 33);
+            this.btnPlot.TabIndex = 18;
+            this.btnPlot.Text = "畫圖";
+            this.btnPlot.UseVisualStyleBackColor = true;
+            this.btnPlot.Click += new System.EventHandler(this.btnPlot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 644);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPlot;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.ComboBox cmbRange;
+        private System.Windows.Forms.ComboBox cmbSymbol;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.NumericUpDown nudPrice;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox txtSymbol;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace StockPriceRecord
+{
+    public partial class Form1 : Form
+    {
+        public class StockPrice
+        {
+            public string Symbol { get; set; }
+            public DateTime Date { get; set; }
+            public decimal Price { get; set; }
+
+            public StockPrice(string symbol, DateTime date, decimal price)
+            {
+                Symbol = symbol;
+                Date = date;
+                Price = price;
+            }
+        }
+
+        private readonly List<StockPrice> _data = new List<StockPrice>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitUi();
+            InitChart();
+            RefreshGridAndSymbols();
+        }
+
+        private void InitUi()
+        {
+            dtpDate.Format = DateTimePickerFormat.Short;
+
+            cmbRange.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbRange.Items.AddRange(new object[] { "7天", "30天", "90天", "1年", "自訂" });
+            cmbRange.SelectedIndex = 1;
+
+            dtpFrom.Format = DateTimePickerFormat.Short;
+            dtpTo.Format = DateTimePickerFormat.Short;
+
+            cmbSymbol.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void InitChart()
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            var area = new ChartArea("Main");
+            area.AxisX.LabelStyle.Format = "yyyy-MM-dd";
+            area.AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+            area.AxisX.MajorGrid.Enabled = false;
+            area.AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dot;
+            chart1.ChartAreas.Add(area);
+
+            chart1.Legends.Clear();
+            chart1.Legends.Add(new Legend("Legend"));
+        }
+
+        // === 手動新增 ===
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var symbol = (txtSymbol.Text ?? "").Trim().ToUpperInvariant();
+            if (string.IsNullOrWhiteSpace(symbol))
+            {
+                MessageBox.Show("請輸入股票代號(Symbol)。");
+                return;
+            }
+
+            var date = dtpDate.Value.Date;
+            var price = nudPrice.Value;
+
+            _data.Add(new StockPrice(symbol, date, price));
+            RefreshGridAndSymbols();
+        }
+
+        // === 匯入 CSV ===
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            using (var ofd = new OpenFileDialog
+            {
+                Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*",
+                Title = "選擇要匯入的 CSV"
+            })
+            {
+                if (ofd.ShowDialog() != DialogResult.OK) return;
+
+                try
+                {
+                    var imported = ImportCsv(ofd.FileName);
+                    _data.AddRange(imported);
+                    RefreshGridAndSymbols();
+                    MessageBox.Show($"匯入完成：{imported.Count} 筆");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("匯入失敗：" + ex.Message);
+                }
+            }
+        }
+
+        private List<StockPrice> ImportCsv(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            if (lines.Length <= 1) return new List<StockPrice>();
+
+            // 期待表頭：Symbol,Date,Price
+            var result = new List<StockPrice>();
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var line = lines[i].Trim();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var parts = line.Split(',');
+                if (parts.Length < 3) continue;
+
+                var symbol = parts[0].Trim().ToUpperInvariant();
+                var dateStr = parts[1].Trim();
+                var priceStr = parts[2].Trim();
+
+                DateTime date;
+                var formats = new[] { "yyyy-MM-dd", "yyyy/MM/dd", "yyyy/M/d" };
+
+                if (!DateTime.TryParseExact(dateStr, formats, CultureInfo.InvariantCulture,
+                    DateTimeStyles.None, out date))
+                {
+                    throw new FormatException($"第 {i + 1} 行 Date 格式錯誤（支援 yyyy-MM-dd 或 yyyy/MM/dd）：{dateStr}");
+                }
+
+
+                decimal price;
+                if (!decimal.TryParse(priceStr, NumberStyles.Any, CultureInfo.InvariantCulture, out price))
+                {
+                    throw new FormatException($"第 {i + 1} 行 Price 格式錯誤：{priceStr}");
+                }
+
+                
+
+                result.Add(new StockPrice(symbol, date.Date, price));
+            }
+            return result;
+        }
+
+        // === 畫圖 ===
+        private void btnPlot_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("btnPlot_Click 有進來");
+
+            if (cmbSymbol.SelectedItem == null)
+            {
+                MessageBox.Show("請先選股票");
+                return;
+            }
+
+            var symbol = cmbSymbol.SelectedItem.ToString();
+            var from = dtpFrom.Value.Date;
+            var to = dtpTo.Value.Date;
+
+            var points = _data
+                .Where(x => x.Symbol == symbol && x.Date >= from && x.Date <= to)
+                .OrderBy(x => x.Date)
+                .ToList();
+
+            MessageBox.Show($"符合條件筆數: {points.Count}\n範圍: {from:yyyy-MM-dd} ~ {to:yyyy-MM-dd}");
+
+            Plot(symbol, points);
+        }
+
+
+        private (DateTime from, DateTime to) GetRange()
+        {
+            var to = DateTime.Today;
+            var from = to.AddDays(-30);
+
+            var selected = cmbRange.SelectedItem?.ToString() ?? "30天";
+            if (selected == "7天") from = to.AddDays(-7);
+            else if (selected == "30天") from = to.AddDays(-30);
+            else if (selected == "90天") from = to.AddDays(-90);
+            else if (selected == "1年") from = to.AddYears(-1);
+            else if (selected == "自訂")
+            {
+                from = dtpFrom.Value.Date;
+                to = dtpTo.Value.Date;
+                if (from > to) (from, to) = (to, from);
+            }
+
+            return (from.Date, to.Date);
+        }
+
+        private void Plot(string symbol, List<StockPrice> points)
+        {
+            chart1.Series.Clear();
+
+            var s = new Series(symbol)
+            {
+                ChartType = SeriesChartType.Line,
+                XValueType = ChartValueType.Date,
+                BorderWidth = 2,
+                MarkerStyle = MarkerStyle.Circle,
+                MarkerSize = 5
+            };
+
+            foreach (var p in points)
+            {
+                s.Points.AddXY(p.Date, p.Price);
+            }
+
+            chart1.Series.Add(s);
+            chart1.ChartAreas["Main"].RecalculateAxesScale();
+
+            if (points.Count == 0)
+            {
+                MessageBox.Show("該期限內沒有資料可畫圖。");
+            }
+        }
+
+        // === Grid + Symbol 下拉更新 ===
+        private void RefreshGridAndSymbols()
+        {
+            grid.AutoGenerateColumns = true;
+            grid.DataSource = _data
+                .OrderByDescending(x => x.Date)
+                .ThenBy(x => x.Symbol)
+                .ToList();
+
+            var symbols = _data.Select(x => x.Symbol).Distinct().OrderBy(x => x).ToList();
+            var current = cmbSymbol.SelectedItem?.ToString();
+
+            cmbSymbol.Items.Clear();
+            foreach (var s in symbols) cmbSymbol.Items.Add(s);
+
+            if (current != null && symbols.Contains(current)) cmbSymbol.SelectedItem = current;
+            else if (symbols.Count > 0) cmbSymbol.SelectedIndex = 0;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
